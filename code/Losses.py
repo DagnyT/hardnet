5136d96bf41eeada1ee2fd992fa700d97d4d1846
johnnysclai <25567299+johnnysclai@users.noreply.github.com>
2018-05-10 02:46:51 +0800

    Update distance_matrix_vector
    A little bit faster

--- code/Losses.py
+++ code/Losses.py
@@ -9,8 +9,7 @@ def distance_matrix_vector(anchor, positive):
     d2_sq = torch.sum(positive * positive, dim=1).unsqueeze(-1)
 
     eps = 1e-6
-    return torch.sqrt((d1_sq.repeat(1, positive.size(0)) + torch.t(d2_sq.repeat(1, anchor.size(0)))
-                      - 2.0 * torch.bmm(anchor.unsqueeze(0), torch.t(positive).unsqueeze(0)).squeeze(0))+eps)
+    return torch.sqrt(2.0 - 2.0 * (anchor @ torch.transpose(positive, 1, 0)) + eps)
 
 def distance_vectors_pairwise(anchor, positive, negative = None):
     """Given batch of anchor descriptors and positive descriptors calculate distance matrix"""
